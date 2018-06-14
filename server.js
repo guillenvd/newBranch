fcdd9844847ded09508cee94e0b724a5fb5a1742
David Guillen <david.guillen@onestop.com>
2018-06-14 00:23:51 -0700

    Routes for user API

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express'); // Call the Express libs
 const mongoose =  require('mongoose');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const post = require('./routes/api/post');
+
 const app = express(); // Use a express
 
 
 // DB CONF
 const db = require('./config/keys').mongoURI;
+
 
 // Connect to MongoDB using Mongoose
 mongoose
@@ -14,6 +20,13 @@ mongoose
 
 app.get('/', (req, res) => res.send('hello!!!')); //router to a homepage
  
+// USE ROUTES
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/post', post);
+
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, ()=> console.log('Server running on port ', port));
